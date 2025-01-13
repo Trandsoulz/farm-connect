62257fe6e9e379ae986abf796669dadd1da62f58
Nnaemeka Bethrand <bethrand2019@gmail.com>
2025-01-13 08:05:34 +0100

    [FIX] : Refactored user model to use ESM

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,38 +1,38 @@
 // Create the user-model
-const mongoose = require('mongoose');
-const validator = require('validator');
+import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'User name is required'],
-        },
-
-        email: {
-            type: String,
-            required: [true, "User email is required"],
-            unique: true,
-            validate: [validator.isEmail, "Please enter a valid email"]
-        },
-        password: {
-            type: String,
-            required: [true, 'Please add a password'],
-            minLength: 8
-         },
-         passwordConfirm: {
-            type: String,
-            required: [true, 'User password confirmation is required'],
-            minLength: 8
-         }
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
     },
 
-    {
-        timestamps: true,
-    }
-)
+    email: {
+      type: String,
+      required: [true, "User email is required"],
+      unique: true,
+      validate: [validator.isEmail, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minLength: 8,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "User password confirmation is required"],
+      minLength: 8,
+    },
+  },
 
-const User = mongoose.model('User', userSchema);
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
 
